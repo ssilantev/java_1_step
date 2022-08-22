c0f4b0ad5547cf7606f1fda57ae6a38ad69b2aae
ssilantev <un.realgar@yandex.ru>
2022-08-22 09:25:23 +0700

    add

---
+++ Average.java
@@ -0,0 +1,10 @@
+public class Average {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+
+        double average = (double) (a + b) / 2;
+        System.out.println("Average = " + average);
+
+    }
+}
